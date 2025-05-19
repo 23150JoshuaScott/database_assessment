7216629718d6394ce8fcf1c4864c1b880579b6da
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-19 13:49:52 +1200

    added basic comments as well as making the displayed data more neat

--- valorant_pros.py
+++ valorant_pros.py
@@ -1,21 +1,23 @@
+#imports
 import sqlite3
-
+#constants and varibles
 DATABASE = "valorant_pros.db"
 
-def all_info():
-    with sqlite3.connect(DATABASE) as db:
-        cursor = db.cursor()
-        sql = "SELECT player.player_name, team.team_name, region.region_name FROM player " \
-        "INNER JOIN team ON player.team_id=team.team_id " \
-        "INNER JOIN region ON team.region_id = region.region_id" \
-        "ORDER BY player.player_id ASCS"
-        cursor.execute(sql)
-        results = cursor.fetchall()
+#functions
+def print_all_info():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = """SELECT player.player_name, player.player_age, player.player_nationality, player.player_edpi, player.player_prize_money, team.team_name, region.region_name 
+    FROM player
+    INNER JOIN team ON player.team_id=team.team_id
+    INNER JOIN region ON team.region_id = region.region_id
+    ORDER BY player.player_id ASC
+    """
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for player in results:
+        print(f"{player[0]:<11}{player[1]:<4}{player[2]:<13}{player[3]:<5}{player[4]:<8}{player[5]:<6}{player[6]}")
+    db.close()
 
-        for thing in results:
-        
-
-print("In my database i have ")
-
-if __name__ == "__main__":
-    print_all_info()
+#main code
+print_all_info()
