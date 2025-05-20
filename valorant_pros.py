dcf104406f6335121c81cb8cb7abd484cd61ea2a
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-21 10:02:24 +1200

    finished all 4 functions

--- valorant_pros.py
+++ valorant_pros.py
@@ -3,7 +3,7 @@ import sqlite3
 #constants and varibles
 DATABASE = "valorant_pros.db"
 
-#functions
+#my first function to print all of the info in my database
 def print_all_info():
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
@@ -20,6 +20,7 @@ def print_all_info():
         print(f"{player[0]:<11}{player[1]:<4}{player[2]:<13}{player[3]:<5}{player[4]:<8}{player[5]:<6}{player[6]}")
     db.close()
 
+#this function prints all of the info from a chosen team
 def print_all_team_info():
     team = input("What team's info would you like to see? \n")
     db = sqlite3.connect(DATABASE)
@@ -33,11 +34,12 @@ def print_all_team_info():
     """
     cursor.execute(sql,(team,))
     results = cursor.fetchall()
-    print("Name       Age Nationality  Edpi USD Won Team  Region")
+    print("\nHere is the info for {team}\nName       Age Nationality  Edpi USD Won Team  Region")
     for player in results:
         print(f"{player[0]:<11}{player[1]:<4}{player[2]:<13}{player[3]:<5}{player[4]:<8}{player[5]:<6}{player[6]}")
     db.close()
 
+#this function prints all of the info from a chosen region
 def print_all_region_info():
     region = input("What regions's info would you like to see? \n")
     db = sqlite3.connect(DATABASE)
@@ -56,9 +58,36 @@ def print_all_region_info():
         print(f"{player[0]:<11}{player[1]:<4}{player[2]:<13}{player[3]:<5}{player[4]:<8}{player[5]:<6}{player[6]}")
     db.close()
 
+def print_all_info_ordered():
+    order_input = input("What would you like to order the data by? \n1. Name\n2. Age\n3. Nationality\n4. Edpi\n5. Prize Money\n")
+    if order_input == "1":
+        order = "player.player_name"
+    elif order_input == "2":
+        order = "player.player_age"
+    elif order_input == "3":
+        order = "player.player_nationality"
+    elif order_input == "4":
+        order = "player.player_edpi"
+    elif order_input == "5":
+        order = "player.player_prize_money"
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = f"""SELECT player.player_name, player.player_age, player.player_nationality, player.player_edpi, player.player_prize_money, team.team_name, region.region_name 
+    FROM player
+    INNER JOIN team ON player.team_id=team.team_id
+    INNER JOIN region ON team.region_id = region.region_id
+    ORDER BY {order} DESC
+    """
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print(f"\nName       Age Nationality  Edpi USD Won Team  Region")
+    for player in results:
+        print(f"{player[0]:<11}{player[1]:<4}{player[2]:<13}{player[3]:<5}{player[4]:<8}{player[5]:<6}{player[6]}")
+    db.close()
+
 #main code
 while True:
-    user_input = input("\nWhat would you like to do?\n1. Press 1 to print all data\n2. Chose a team to see their info\n3. Chose a region to see their info\n")
+    user_input = input("\nWhat would you like to do?\n1. Press 1 to print all data\n2. Chose a team to see their info\n3. Chose a region to see their info\n4. Chose a peice of data to oder by\n")
     if user_input == "exit":
         break
     elif user_input == "1":
@@ -67,5 +96,7 @@ while True:
         print_all_team_info()
     elif user_input == "3":
         print_all_region_info()
+    elif user_input == "4":
+        print_all_info_ordered()
     else:
         print("You have not chosen an option\n")
